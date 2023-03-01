87e0a0175cf1f685f5f70bcace3bd03d82e12cdf
Maxim Zatrutsin <63240231+MaximZatrutsin@users.noreply.github.com>
2023-03-02 00:50:59 +0300

    Update AsyncArray.mjs
    создание

---
+++ src/AsyncArray.mjs
@@ -0,0 +1,10 @@
+class AsyncArray extends Array {
+   async serialMap(callback) {
+     const result = new AsyncArray();
+     for (let i = 0; i < this.length; i++) {
+       const mappedItem = await callback(this[i], i, this);
+       result.push(mappedItem);
+     }
+     return result;
+   }
+ }
